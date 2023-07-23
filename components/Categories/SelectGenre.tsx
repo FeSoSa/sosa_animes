10b880ad11e604c82be63fcb8670750f4df8c9b7
FeSoSa <felipesouzasalles@gmail.com>
2023-07-23 14:50:16 -0300

    fix

--- components/Categories/SelectGenre.tsx
+++ components/Categories/SelectGenre.tsx
@@ -1,10 +1,11 @@
 import { RadioGroup } from "@headlessui/react";
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
 import { GenreByBR, GenreByEN } from "../../constants/GenreBy";
 import { useRouter } from "next/router";
 
 import { GoCheck } from 'react-icons/go'
 import { CheckCircleIcon, CheckCircleFillIcon } from '@primer/octicons-react'
+import { Context } from "../../contexts/ContextProvider";
 
 interface Props {
   genre: string,
@@ -16,6 +17,7 @@ export default function SelectGenre({ genre, setGenre, lang }: Props) {
 
   const router = useRouter()
   const { type, page } = router.query as { type: string, page: string }
+  const {language} = useContext(Context)
 
   const [selectedLang,setSelectedLang] = useState(GenreByBR)
   useEffect(() => {
@@ -32,7 +34,7 @@ export default function SelectGenre({ genre, setGenre, lang }: Props) {
         {selectedLang.map((i, index) =>
 
           <RadioGroup.Option key={index} value={type == 'movie' ? i.movie||i.value : i.value}
-            onClick={() => router.push(`/${type}/${type == 'movie' ? i.movie||i.value : i.value}/${page}`)}
+            onClick={() => router.push(`/${language}}/${type}/${type == 'movie' ? i.movie||i.value : i.value}/${page}`)}
             className={`cursor-pointer flex justify-between text-md rounded items-center p-3 mt-2 hover:bg-[#FFD966]
             ${i.value == genre || i.movie == genre ? 'bg-[#222222] text-white hover:bg-[#333333]' : 'bg-white text-black'}`}
           >
