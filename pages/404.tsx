8873566540ecac1c020977359f65c3fd94640e99
Felipe Salles <felipesouzasalles@gmail.com>
2025-10-22 19:21:03 -0300

    Update 2.1.1

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Context } from "../contexts/ContextProvider";
 
 export default function Custom404() {
+  const { language } = useContext(Context);
 
-const {language} = useContext(Context)
-
-    return (
-        <div className="w-screen h-screen flex items-center justify-center flex-col">
-            <h1 className="font-bold">Parece que você se perdeu no limbo dos Animes</h1>
-            <p>Vamos voltar? <Link href={`/${language}`} className="text-yellow font-bold">clique aqui</Link></p>
-        </div>
-    )
+  return (
+    <div className="w-screen h-screen flex items-center justify-center flex-col">
+      <h1 className="font-bold">
+        Parece que você se perdeu no limbo dos Animes :O
+      </h1>
+      <p>
+        Vamos voltar?{" "}
+        <Link href={`/${language}`} className="text-yellow font-bold">
+          clique aqui
+        </Link>
+      </p>
+    </div>
+  );
 }
